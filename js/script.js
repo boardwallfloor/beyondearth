f0b47dc90a3970a7b73353f094b713ca07870a98
boardwallfloor <xanthibumi@gmail.com>
2019-12-03 06:34:27 +0700

    alpha 0.2

--- js/script.js
+++ js/script.js
@@ -1,19 +1,38 @@
- 
- document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems,"right");
-    console.log("output");
-  });
-
   document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.menu');
-    var instances = M.Dropdown.init(elems);
+    var options = {
+    	'closeOnClick' : false
+    }
+    var instances = M.Dropdown.init(elems, options);
   });
   document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.logbook');
-    var instances = M.Dropdown.init(elems);
+    var options = {
+    	'closeOnClick' : false
+    }
+    var instances = M.Dropdown.init(elems,options);
   });
 
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems);
+  });
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.comet');
+    var instances = M.Modal.init(elems);
+  });
+
+ //  const input = document.getElementById("autocomplete-input")
+ //  input.addEventListener("keyup", function(event) {
+	//   // Number 13 is the "Enter" key on the keyboard
+	//   if (event.keyCode === 13) {
+	//     // Cancel the default action, if needed
+	//  		console.log("te");
+	//     // Trigger the button element with a click
+	//     var elems = document.querySelectorAll('.modal');
+ //    var instances = M.Modal.init(elems);
+	//   }
+	// }); 
   // Initialize collapsible (uncomment the lines below if you use the dropdown variation)
   // var collapsibleElem = document.querySelector('.collapsible');
   // var collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
